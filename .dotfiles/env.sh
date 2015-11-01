2843acd177363458598580a52591fab3605189c8
Michael Nigh <mic.nigh@gmail.com>
2015-11-01 12:30:02 -0800

    Add npm bin path

--- .dotfiles/env.sh
+++ .dotfiles/env.sh
@@ -4,6 +4,7 @@ export PATH=/cmd:$PATH
 export TERM=cygwin
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
+export PATH="$PATH:$HOME/npm/bin"
 
 . .dotfiles/env/docker.sh
 . .dotfiles/env/git.sh
