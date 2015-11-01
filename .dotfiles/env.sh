162106d47c4da48298f75fa2c03d91f5dc877294
Michael Nigh <mic.nigh@gmail.com>
2015-11-01 13:44:09 -0800

    Update paths for npm

--- .dotfiles/env.sh
+++ .dotfiles/env.sh
@@ -5,6 +5,7 @@ export TERM=cygwin
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
 export PATH="$PATH:$HOME/npm/bin"
+export PATH="$PATH:$HOME/npm"
 
 . .dotfiles/env/docker.sh
 . .dotfiles/env/git.sh
