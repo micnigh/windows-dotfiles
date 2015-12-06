33d05ea4ab34378e993893e774277d78be66924d
Michael Nigh <mic.nigh@gmail.com>
2015-12-06 15:38:11 -0800

    Add completion script - disabled for now

---
+++ .dotfiles/completion.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# docker bash completions
+# currently too slow to enable - also depends on `bash-completions` project
+# which doesn't seem to work in windows :(
+#
+# if [ ! -f ~/.dotfiles/completion/docker.sh ]; then
+#   curl \
+#     -o ~/.dotfiles/completion/docker.sh \
+#     --insecure \
+#     https://raw.githubusercontent.com/docker/docker/master/contrib/completion/bash/docker
+# fi
+#
+# . ~/.dotfiles/completion/docker.sh
