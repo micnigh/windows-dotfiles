017f24215aa6359acf57873dfbf925b9e0a8ff09
Michael Nigh <mic.nigh@gmail.com>
2016-02-11 21:05:51 -0800

    revert disabling msys path mangling
    - better to disable on case by case basis

--- .dotfiles/env/msys.sh
+++ .dotfiles/env/msys.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-export MSYS_NO_PATHCONV=1 # prevent msys from mangling all / paths to windows equivalents
+# uncomment to disable mangling windows paths
+# some tools may fail to work without path mangling in msys, such as node
+# recommended to disable explicitly instead by preprending ENV VAR to commands
+# export MSYS_NO_PATHCONV=1 # prevent msys from mangling all / paths to windows equivalents
